0285819da7ebba32cf6580b338ae12294734c679
zvandivort <zlvandivort2393@gmail.com>
2018-04-05 17:19:49 -0500

    UGH

--- main.js
+++ main.js
@@ -22,3 +22,34 @@ function fetchIssues() {
             '</div>';
     }
 }
+
+function saveIssue(e) {
+    var issueId = chance.guid();
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueStatus = 'Open';
+    var issue = {
+      id: issueId,
+      description: issueDesc,
+      severity: issueSeverity,
+      assignedTo: issueAssignedTo,
+      status: issueStatus
+    }
+    
+    if (localStorage.getItem('issues') === null) {
+      var issues = [];
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+      var issues = JSON.parse(localStorage.getItem('issues'));
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
+    }
+    
+    document.getElementById('issueInputForm').reset();
+   
+    fetchIssues();
+    
+    e.preventDefault(); 
+   }
